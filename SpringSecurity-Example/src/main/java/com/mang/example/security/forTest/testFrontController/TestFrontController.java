9b7783856011fb5ba3f2ffaa0415a1f6d90fb029
whffu762 <whffu762@naver.com>
2024-08-14 18:32:09 +0900

    dispatcherServlet - how to retrieve right handler

--- SpringSecurity-Example/src/main/java/com/mang/example/security/forTest/testFrontController/TestFrontController.java
+++ SpringSecurity-Example/src/main/java/com/mang/example/security/forTest/testFrontController/TestFrontController.java
@@ -1,11 +1,17 @@
 package com.mang.example.security.forTest.testFrontController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,22 +19,45 @@ public class TestFrontController {
 
     private final ApplicationContext applicationContext;
 
-    @ResponseBody
-    @GetMapping("/front")
-    public String[] getAllBeans(){
+    @GetMapping("/test/front")
+    public String front(){
 
-        String [] allBeans = applicationContext.getBeanDefinitionNames();
-        System.out.println(applicationContext.getClass());
-
-        return new String [] {"ok", "hello"};
+        return "/test/front";
     }
 
-    @GetMapping("/front2")
-    public String getAllBeans2(){
+    @PostMapping(value = "/test/front", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String front2(@RequestParam("inputImage")MultipartFile image, Model model) throws IOException {
 
-        String [] allBeans = applicationContext.getBeanDefinitionNames();
-        System.out.println(applicationContext.getClass());
+        String base64Image = Base64Utils.encodeToString(image.getBytes());
+        model.addAttribute("image", base64Image);
+        return "/test/front";
+    }
 
-        return "/user/login";
+    @PostMapping(value = "/test/front", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String front2(@RequestParam("imageName") String name, Model model){
+
+        model.addAttribute("imageName", name);
+        return "/test/front";
+    }
+
+    @GetMapping(value="/test/front", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> front3() {
+
+        String data = "{\"name\" : \"hello\"}";
+        return ResponseEntity.ok().body(data);
+    }
+
+    @GetMapping(value="/test/front", params = "data")
+    public String front(@RequestParam("data") String data, Model model){
+
+        model.addAttribute("data", data);
+        return "/test/front";
+    }
+
+    @GetMapping(value="/test/front/{msg}")
+    public String front4(@PathVariable("msg") String data, Model model){
+
+        model.addAttribute("data2", data);
+        return "/test/front";
     }
 }
